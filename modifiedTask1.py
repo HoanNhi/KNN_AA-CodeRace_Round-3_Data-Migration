80e41dc217e6b3c7a2cccba4f3110b2378a5469f
Khoa-Nguyen-Kevin <khoanguyenduydang1980@gmail.com>
2023-06-07 03:28:01 +0700

    Second commit

--- modifiedTask1.py
+++ modifiedTask1.py
@@ -7,38 +7,47 @@ import sys
 
 #input_file_path = sys.argv[1]
 input_file_path = "Requirements.reqif"
-config_file_path = "testconfig2.json"
+config_file_path = "testconfig3.json"
 data = {}
 
 
 def getArtifactAttributeByName(reqif_bundle, obj, attributeName, customValues=None):
+    """
+    Return value of an artifact's attribute
+    """
     value = ""
+    #Traverse through all attributes
     for attribute in obj.attributes:
-            ref = attribute.definition_ref
-            name = reqif_bundle.lookup.spec_types_lookup[obj.spec_object_type].attribute_map[ref].long_name
+            ref = attribute.definition_ref #Get reference 
+            name = reqif_bundle.lookup.spec_types_lookup[obj.spec_object_type].attribute_map[ref].long_name #Get name of attribute
             if name == attributeName:
-                if attribute.attribute_type.name == "ENUMERATION": #Nếu attribute là enum
+                if attribute.attribute_type.name == "ENUMERATION": #If the attribute type is Enum
                     Ref = attribute.definition_ref
                     ValueRef = attribute.value[0]
                     T_Ref = reqif_bundle.lookup.spec_types_lookup[obj.spec_object_type].attribute_map[Ref].datatype_definition
                     value = reqif_bundle.lookup.data_types_lookup[T_Ref].values_map[ValueRef].long_name
-                else:
+                else: #If the attribute type is anything else
                     value = attribute.value
 
-    if customValues != None:
+    if customValues != None: #If there were specified custom values
         if value in customValues.keys():
             value = customValues[value]
         else:
             value = ""
     return value
 
-def getArtifactAttributes(reqif_bundle, obj, config:dict): #FIXX
+def getArtifactAttributes(reqif_bundle, obj, config:dict):
+    """
+    Return a dictionary object containing all required attributes of an artifact.\n
+    The required attributes to extract are read from config.
+    """
     attributeDict = {}
     for key, value in config.items(): 
-        if value["Source"] == "ArtifactType": #Trường hợp đặc biệt, vì type của artifacts không thuộc các attributes
+        if value["Source"] == "ArtifactType": #Artifact type is a special case, because it is not an attribute
             attributeDict[key] = reqif_bundle.get_spec_object_type_by_ref(obj.spec_object_type).long_name
             continue
-        if value["MappingType"] == "1-1":
+
+        if value["MappingType"] == "1-1": 
             attributeDict[key] = getArtifactAttributeByName(reqif_bundle, obj, value["Source"])
             continue
         if value["MappingType"] == "Custom":
@@ -69,23 +78,26 @@ def getArtifactAttributes(reqif_bundle, obj, config:dict): #FIXX
             attributeNames[value["Source"]] = 0
 
 def getModuleAttributeByName(reqif_bundle, obj, attributeName, customValues=None):
-    if attributeName == "ModuleName":
+    """
+    Return value of a specification(module)'s attribute."""
+    if attributeName == "ModuleName": #Special case
         value = obj.long_name
-    if attributeName == "ModuleType":
+    if attributeName == "ModuleType": #Special case
         value = reqif_bundle.lookup.spec_types_lookup[obj.specification_type].long_name
 
     for attribute in obj.values:
             ref = attribute.definition_ref
             name = reqif_bundle.lookup.spec_types_lookup[obj.specification_type].spec_attribute_map[ref]
             if name == attributeName:
-                if attribute.attribute_type.name == "ENUMERATION": #Nếu attribute là enum
+                if attribute.attribute_type.name == "ENUMERATION": #If attribute type is enum
                     Ref = attribute.definition_ref
                     ValueRef = attribute.value[0]
                     T_Ref = reqif_bundle.lookup.spec_types_lookup[obj.spec_object_type].attribute_map[Ref].datatype_definition
                     value = reqif_bundle.lookup.data_types_lookup[T_Ref].values_map[ValueRef].long_name
                 else:
                     value = attribute.value
-    if customValues != None:
+    
+    if customValues != None: #If there were custom values specified
         if value in customValues.keys():
             value = customValues[value]
         else:
@@ -93,6 +105,8 @@ def getModuleAttributeByName(reqif_bundle, obj, attributeName, customValues=None
     return value
 
 def reqIFtoJSON(input_file_path, config_file_path):
+
+    #Opening config file
     try:
        file = open(config_file_path,"r")
     except PermissionError:
@@ -101,17 +115,28 @@ def reqIFtoJSON(input_file_path, config_file_path):
     except FileNotFoundError:
         print("Config file not found!")
         return None
-    
+    #Load json object from config file
     configStructure = json.load(file)
+    #Get reqif object from the parser
     reqif_bundle = ReqIFParser.parse(input_file_path)
     jsonObj = {}
 
     for specification in reqif_bundle.core_content.req_if_content.specifications:
+        #Get the hierachy including all artifacts
         hierachy = reqif_bundle.iterate_specification_hierarchy(specification)
 
         for key, value in configStructure.items():
             if value["MappingType"] == "1-1":
-                jsonObj[key] = getModuleAttributeByName(reqif_bundle,specification, value["Source"])
+                if isinstance(value["Source"], dict): #Get attributes of artifact
+                    config = value["Source"]
+                    hierachy_obj = next(hierachy, None)
+                    if hierachy_obj == None: #There are no more artifacts left in this specification
+                        break
+                    obj = reqif_bundle.get_spec_object_by_ref(hierachy_obj.spec_object)
+                    attributesDict = getArtifactAttributes(reqif_bundle, obj, config)
+                    jsonObj[key] = attributesDict
+                else: #Get attribute of specification
+                    jsonObj[key] = getModuleAttributeByName(reqif_bundle,specification, value["Source"])
             
             if value["MappingType"] == "Custom":
                 customValues = value.get("CustomValues")
@@ -124,7 +149,7 @@ def reqIFtoJSON(input_file_path, config_file_path):
                     if hierachy_obj == None: #There are no more artifacts left in this specification
                         break
                     obj = reqif_bundle.get_spec_object_by_ref(hierachy_obj.spec_object)
-                    config = value["Source"][artifactCount]
+                    config = value["Source"][artifactCount] #Get config of current artifact
                     attributesDict = getArtifactAttributes(reqif_bundle, obj, config)
                     ArtifactList.append(attributesDict)
                 jsonObj[key] = ArtifactList
---
+++ modifiedTask2.py
@@ -0,0 +1,142 @@
+from rstcloth import RstCloth
+import json
+import sys
+import html2text
+import htmltools
+import re
+
+#input_file_path = sys.argv[1]
+input_file_path = "output_task1_R3.json"
+config_file_path = "testconfig3.json"
+output_file_path = "output_task2_R3.rst"
+
+def replace_newline(text):
+    """
+    A function to replace any <br> tag with "\n|". 
+    Input: A converted text from html code
+    Output: The processed text
+    """
+    pattern = r'(?<!\n)\n(?!\n)|(?<!\n)\n\\(?!\n)'
+    replaced_text = re.sub(pattern, '\n| ', text)
+    replaced_text = replaced_text.replace("|\\", "| ")
+    return replaced_text
+
+def decodeHTML(htmlText):
+    """
+    A function to decode HTML scripts to text
+    """
+    text_HTML = html2text.html2text(htmlText)
+    processed_text = replace_newline(text_HTML)
+    lines = processed_text.split("\n") #Split the html data into different parts, separating by escape sequence "\n"
+    return lines
+
+def sortAttributes(artifact, artifactDict):
+    """
+    Sort all attributes of an artifact into its corresponding placement, 
+    based on config (artifactDict)
+
+    Returns: Three lists indicating value texts, content texts and sub-directive texts
+    """
+    valueTexts = []
+    contentTexts = []
+    sub_directiveTexts = []
+
+    for attribute in artifact.keys():
+        placement = artifact[attribute].get("Placement", None) #Read Placement value in config
+        if placement == None or placement == "ValueText":
+            valueTexts.append((attribute, artifactDict[attribute]))
+        elif placement == "Content":
+            str = artifactDict[attribute]
+            str = decodeHTML(str) #If it is content, we need to decode HTML
+            contentTexts.append(str)
+        elif placement == "Sub-directive":
+            sub_directiveTexts.append((attribute, artifactDict[attribute]))
+    
+    return valueTexts,contentTexts,sub_directiveTexts
+
+def writeJSONtoRst(input_file_path, config_file_path, output_file_path):
+    #Open neccesary files
+    try:
+        input_file = open(input_file_path, "r")
+        config_file = open(config_file_path, "r")
+        output_file = open(output_file_path, "w")
+    except PermissionError:
+        print("No permision to open file!")
+        return False
+    
+    #Load json object from file
+    input_json = json.load(input_file)
+    config_json = json.load(config_file)
+    #Initialize Rst parser
+    rstData = RstCloth(output_file)
+
+    #Write heading (First item of input)
+    for headingKey, headingValue in config_json.items():
+        if headingValue["MappingType"] == "1-1" or headingValue["MappingType"] == "Custom":
+            if isinstance(headingValue["Source"], dict): #If item is an artifact
+                str = input_json[headingKey].values()[0]
+            else: #If item is specification attribute
+                str = input_json[headingKey]
+            rstData.title(str)
+            rstData.newline()
+            break
+        else: #Array mapping?
+            break
+        
+    check = 1
+    #Traverse through all items (artifacts) in input json
+    for key, value in config_json.items():
+        if check == 1: #Just to skip the first element, which is the heading we've already written
+            check = 0
+            continue
+        #Write sub-headings
+        if value["MappingType"] == "1-1" or value["MappingType"] == "Custom":
+            if isinstance(value["Source"], dict): #If source is an artifact, we take only the first attribute as sub-heading
+                str = list(input_json[key].values())[0]
+            else:
+                str = input_json[key]
+            
+            str = decodeHTML(str) 
+            for line in str:
+                if line == '': #Skip empty lines
+                    continue
+                rstData.heading(line,"*")
+            rstData.newline()
+            continue
+
+        #Write directives
+        if value["MappingType"] == "Array":
+            array_json = input_json[key]
+            artifactCount = 0
+            for artifact in value["Source"]:
+                #Initialize three lists for three possible placements 
+                valueTexts = []
+                contentTexts = []
+                sub_directiveTexts = []
+                
+                #Get config of artifact (in the form of dictionary)
+                artifactDict = array_json[artifactCount]
+                artifactCount += 1
+                
+                #Decide which attributes belong to which placement
+                valueTexts, contentTexts, sub_directiveTexts = sortAttributes(artifact, artifactDict)
+                
+                #Write the attributes
+                #Directive + value texts
+                rstData.directive(name="sw_req",fields=valueTexts)
+                rstData.newline()
+                #HTML Content
+                for value in contentTexts:
+                    for line in value:
+                        if line == '':
+                            continue
+                        rstData.content(line)
+                        rstData.newline()
+                #Sub-directives
+                for value in sub_directiveTexts:
+                    rstData.directive(name=value[0],content=value[1])
+                    rstData.newline()
+    print("Data migration complete.\nOutput file is at " + output_file.name)
+    return True
+
+check = writeJSONtoRst(input_file_path, config_file_path, output_file_path)
